a39c362189277e4d21b3828d970bc632f933a22c
vilhelem <60222326+vilhelem@users.noreply.github.com>
2020-03-19 16:24:15 +0100

    asd

---
+++ ecom/resources/templates/front/footer.php
@@ -0,0 +1,15 @@
+<div class="container">
+
+<hr>
+
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; Your Website 2030</p>
+        </div>
+    </div>
+</footer>
+
+</div>
+<!-- /.container -->
