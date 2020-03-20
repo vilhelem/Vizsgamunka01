25f4bff5c6a56a9a0ce3b820ea11a8d24d0ef86f
vilhelem <60222326+vilhelem@users.noreply.github.com>
2020-03-20 10:21:36 +0100

    side nav
    Az oldalsó navigációs panel, már a kategóriák táblából kapja az adatokat.

--- ecom/resources/templates/front/side_nav.php
+++ ecom/resources/templates/front/side_nav.php
@@ -1,10 +1,26 @@
 <div class="col-md-3">
                 <p class="lead">Órasziget</p>
                 <div class="list-group">
-                    <a href="category.html" class="list-group-item">Category 1</a>
-                    <a href="#" class="list-group-item">Category 2</a>
-                    <a href="#" class="list-group-item">Category 3</a>
+
+                <?php 
+                
+                
+                $query = "SELECT * FROM kategoriak";
+                $send_query = mysqli_query($connection, $query);
+if(!$send_query){
+    die("QUERY FAILED" . mysqli_error($connection));
+}
+
+                while($row= mysqli_fetch_array($send_query)){
+
+                  echo  "<a href='' class='list-group-item'>{$row['kat_nev']}</a>";
+
+                }
+                
+                
+                ?>
+
+
                 </div>
             </div>
 
-            
